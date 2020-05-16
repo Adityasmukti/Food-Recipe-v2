f25f0f1d6b051949e66b662399892fdb476cfcbb
Adityas Muktiyanto <adityasmukti@outlook.com>
2020-05-16 14:14:25 +0700

    s

---
+++ application/views/home.php
@@ -0,0 +1,67 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+  <meta charset="utf-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+  <meta name="description" content="">
+  <meta name="author" content="">
+
+  <title>About</title>
+
+  <!-- Header -->
+  <?php $this->load->view('header'); ?>
+  <!-- End Header -->
+</head>
+
+<body id="page-top">
+  <!-- Page Wrapper -->
+  <div id="wrapper">
+    <!-- Sidebar -->
+    <?php $this->load->view('sidebar'); ?>
+    <!-- End of Sidebar -->
+    <!-- Content Wrapper -->
+    <div id="content-wrapper" class="d-flex flex-column">
+      <!-- Main Content -->
+      <div id="content">
+        <!-- Topbar -->
+        <?php $this->load->view('topbar'); ?>
+        <!-- End of Topbar -->
+        <!-- Begin Page Content -->
+        <div class="container-fluid about">
+
+          <!-- Page Heading -->
+          <div class="d-sm-flex align-items-center justify-content-between mb-4">
+            <h1 class="h3 mb-0 text-gray-800">About My Recipe</h1>
+          </div>
+
+          <!-- Card Main Content -->
+          <div class="card">
+            <div class="card-header bg-gradient-primary text-center">
+              <i class="fas fa-laugh-wink"></i>
+              <h1 class="m-0 font-weight-bold mt-3"><?= $this->session->userdata('appname') ?> <?= $this->session->userdata('version') ?></h1>
+            </div>
+            <div class="card-body text-center">
+              <?= htmlspecialchars_decode($this->session->userdata('about')); ?>
+            </div>
+          </div>
+          <!-- End Card Main Content -->
+
+        </div>
+        <!-- /.container-fluid -->
+      </div>
+      <!-- End of Main Content -->
+      <!-- Footer -->
+      <?php $this->load->view('footer'); ?>
+      <!-- End of Footer -->
+    </div>
+    <!-- End of Content Wrapper -->
+  </div>
+  <!-- End of Page Wrapper -->
+  <!-- Script -->
+  <?php $this->load->view('script'); ?>
+  <!-- End Script -->
+</body>
+
+</html>
--- application/views/sidebar.php
+++ application/views/sidebar.php
@@ -1,7 +1,7 @@
-<ul class="navbar-nav bg-secondary sidebar sidebar-black accordion" id="accordionSidebar">
+<ul class=" sidenav navbar-nav bg-gradient-primary sidebar sidebar-black accordion" id="accordionSidebar">
 
   <!-- Sidebar - Brand -->
-  <a class="sidebar-brand d-flex align-items-center justify-content-center" href="<?= base_url() ?>">
+  <a class="sidebar-brand d-flex align-items-center justify-content-center text-white" href="<?= base_url() ?>">
     <!-- <div class="sidebar-brand-icon rotate-n-15">
       <i class="fas fa-laugh-wink"></i>
     </div> -->
@@ -12,32 +12,32 @@
   <!-- Divider -->
   <hr class="sidebar-divider my-0">
 
+  <!-- Nav Item - Category -->
+  <li class="nav-item <?= $this->uri->segment(1) === "category" ? "active shadow" : ""; ?>">
+    <a class="nav-link text-white" href="<?= base_url('category') ?>">
+      <i class="fas fa-fw fa-list text-white"></i>
+      <span>Category</span></a>
+  </li>
+
   <!-- Nav Item - Recipe -->
-  <li class="nav-item <?php echo $this->uri->segment(1) === "recipe" ? "active" : "";
+  <li class="nav-item <?php echo $this->uri->segment(1) === "recipe" ? "active shadow" : "";
                       echo $this->uri->segment(1) === "" ? "active" : ""; ?>">
-    <a class="nav-link" href="<?= base_url('recipe') ?>">
-      <i class="fas fa-fw fa-hamburger"></i>
+    <a class="nav-link text-white" href="<?= base_url('recipe') ?>">
+      <i class="fas fa-fw fa-hamburger text-white"></i>
       <span>Recipe</span></a>
   </li>
 
   <!-- Nav Item - Notification -->
-  <li class="nav-item <?= $this->uri->segment(1) === "notification" ? "active" : ""; ?>">
-    <a class="nav-link" href="<?= base_url('notification') ?>">
-      <i class="fas fa-fw fa-bell"></i>
+  <li class="nav-item <?= $this->uri->segment(1) === "notification" ? "active shadow" : ""; ?>">
+    <a class="nav-link text-white" href="<?= base_url('notification') ?>">
+      <i class="fas fa-fw fa-bell text-white"></i>
       <span>Notification</span></a>
   </li>
 
-  <!-- Nav Item - Category -->
-  <li class="nav-item <?= $this->uri->segment(1) === "category" ? "active" : ""; ?>">
-    <a class="nav-link" href="<?= base_url('category') ?>">
-      <i class="fas fa-fw fa-list"></i>
-      <span>Category</span></a>
-  </li>
-
   <!-- Nav Item - Users -->
-  <li class="nav-item <?= $this->uri->segment(1) === "users" ? "active" : ""; ?>">
-    <a class="nav-link" href="<?= base_url('users') ?>">
-      <i class="fas fa-fw fa-user"></i>
+  <li class="nav-item <?= $this->uri->segment(1) === "users" ? "active shadow" : ""; ?>">
+    <a class="nav-link text-white" href="<?= base_url('users') ?>">
+      <i class="fas fa-fw fa-user text-white"></i>
       <span>Users</span></a>
   </li>
 
@@ -45,30 +45,30 @@
   <hr class="sidebar-divider">
 
   <!-- Heading -->
-  <div class="sidebar-heading">
+  <div class="sidebar-heading text-white">
     Addons
   </div>
 
   <!-- Nav Item - Pages Collapse Menu -->
   <li class="nav-item <?= $this->uri->segment(1) == "settings" ? "active" : ""; ?>">
-    <a class="nav-link <?= $this->uri->segment(2) == "" ? "collapsed" : ""; ?>" href="#" data-toggle="collapse" data-target="#collapsePages" aria-expanded="true" aria-controls="collapsePages">
-      <i class="fas fa-fw fa-wrench"></i>
+    <a class="nav-link text-white <?= $this->uri->segment(2) == "" ? "collapsed" : ""; ?>" href="#" data-toggle="collapse" data-target="#collapsePages" aria-expanded="true" aria-controls="collapsePages">
+      <i class="fas fa-fw fa-wrench text-white"></i>
       <span>Setting</span>
     </a>
-    <div id="collapsePages" class="collapse <?= $this->uri->segment(2) === "" ? "" : "show"; ?>" aria-labelledby="headingPages" data-parent="#accordionSidebar">
-      <div class="bg-white py-2 collapse-inner rounded">
-        <a class="collapse-item <?= $this->uri->segment(2) === "application" ? "active" : ""; ?>" href="<?= base_url('settings/application') ?>">Application</a>
-        <a class="collapse-item <?= $this->uri->segment(2) === "fcm" ? "active" : ""; ?>" href="<?= base_url('settings/fcm') ?>">FCM</a>
-        <a class="collapse-item <?= $this->uri->segment(2) === "registeremail" ? "active" : ""; ?>" href="<?= base_url('settings/registeremail') ?>">Register Email</a>
-        <a class="collapse-item <?= $this->uri->segment(2) === "forgotpassemail" ? "active" : ""; ?>" href="<?= base_url('settings/forgotpassemail') ?>">Forgot Password Email</a>
+    <div id="collapsePages" class="collapse <?= $this->uri->segment(1) === "settings" ? "show" : ""; ?>" aria-labelledby="headingPages" data-parent="#accordionSidebar">
+      <div class="bg-gradient-primary py-2 collapse-inner rounded">
+        <a class="collapse-item text-white <?= $this->uri->segment(2) === "application" ? "active shadow" : ""; ?>" href="<?= base_url('settings/application') ?>">Application</a>
+        <a class="collapse-item text-white <?= $this->uri->segment(2) === "fcm" ? "active shadow" : ""; ?>" href="<?= base_url('settings/fcm') ?>">FCM</a>
+        <a class="collapse-item text-white <?= $this->uri->segment(2) === "registeremail" ? "active shadow" : ""; ?>" href="<?= base_url('settings/registeremail') ?>">Register Email</a>
+        <a class="collapse-item text-white <?= $this->uri->segment(2) === "forgotpassemail" ? "active shadow" : ""; ?>" href="<?= base_url('settings/forgotpassemail') ?>">Forgot Password Email</a>
       </div>
     </div>
   </li>
 
   <!-- Nav Item - About -->
-  <li class="nav-item <?= $this->uri->segment(1) == "about" ? "active" : ""; ?>">
-    <a class="nav-link" href="<?= base_url('about') ?>">
-      <i class="fas fa-fw fa-info"></i>
+  <li class="nav-item <?= $this->uri->segment(1) == "about" ? "active shadow" : ""; ?>">
+    <a class="nav-link text-white" href="<?= base_url('about') ?>">
+      <i class="fas fa-fw fa-info text-white"></i>
       <span>About</span></a>
   </li>
   <!-- Divider -->
